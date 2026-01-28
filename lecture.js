2dfa3bcad4b1dc32791176c9e0980be7ff750cce
shrivastavaanshika30-boop <shrivastavanshika30@gmail.com>
2026-01-28 14:40:24 +0530

    Initial commit

---
+++ lecture.js
@@ -0,0 +1,16 @@
+console.log(a)
+//console.log(b)
+
+var a=678
+let b=567
+
+console.log(a)
+console.log(b)
+
+function print(){
+    let c=63
+    console.log(c)
+    console.log("inside fn")
+}
+
+print()
